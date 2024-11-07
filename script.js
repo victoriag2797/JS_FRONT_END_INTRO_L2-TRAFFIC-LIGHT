934a33386d349e1503f957978d1f454524d92abd
victoriag2797 <victoriag2797@tuckahoeschools.org>
2024-11-07 13:32:51 +0000

    intial commit

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 const redLight = document.querySelector('.red-light');
 const yellowLight = document.querySelector('.yellow-light');
 const greenLight = document.querySelector('.green-light');
-const allLights = document.querySelector('.all-lights')
+const allLights = document.querySelector('.all-lights');
 
 //Use getElementById which allows us to get the ID so that we can add an event listener to the buttons, listen for a click and then set the color via css properties.
 const redSwitch = document.getElementById('red-switch');
 const yellowSwitch = document.getElementById('yellow-switch');
 const greenSwitch = document.getElementById('green-switch');
 const allLightsSwitch = document.getElementById('all-lights-switch');
+const partySwitch = document.getElementById('party-switch');
 
 //Add event listeners that listen for a button click
 redSwitch.addEventListener('click', () => {
@@ -22,16 +23,33 @@ greenSwitch.addEventListener('click', () => {
 })
 
 //Turn all lights on and off with their specific buttons
-redSwitch.addEventListener('click', () => {
-    redLight.style.backgroundColor = 'red';
-})
-yellowSwitch.addEventListener('click', () => {
-    yellowLight.style.backgroundColor = 'yellow';
-})
-greenSwitch.addEventListener('click', () => {
-    greenLight.style.backgroundColor = 'green';
-})
+
 //Party Mode Button
+
+let currentLight = 'magenta';
+let intervalId = null;
+
+function partyLight(){
+    redLight.style.backgroundColor = '#444';
+    yellowLight.style.backgroundColor = '#444';
+    greenLight.style.backgroundColor = '#444';
+    if(currentLight === 'magenta'){
+        redLight.style.backgroundColor = 'magenta'
+        currentLight = 'yellow'
+    } else if(currentLight === 'yellow'){
+            yellowLight.style.backgroundColor = 'yellow'
+            currentLight = 'cyan'
+    }else if(currentLight === 'cyan'){
+        greenLight.style.backgroundColor = 'cyan'
+        currentLight = 'magenta'
+    }
+}
+
+partySwitch.addEventListener('click', () => {
+    if(!intervalId){
+        intervalId = setInterval(partyLight, 100);
+    }
+    });
 
 //Turn all lights on and off at once with a single button
 allLightsSwitch.addEventListener('click', () => {
@@ -39,3 +57,63 @@ allLightsSwitch.addEventListener('click', () => {
     yellowLight.style.backgroundColor = 'yellow';
     greenLight.style.backgroundColor = 'green';
 })
+
+let isRed = true;
+
+redSwitch.addEventListener('click', () => {
+    redLight.style.backgroundColor = 'red';
+    if(isRed){
+    redLight.style.backgroundColor = 'red';
+    } else {
+        redLight.style.backgroundColor = '#444';
+    }
+    isRed = !isRed;
+});
+
+let isYellow = true;
+
+yellowSwitch.addEventListener('click', () => {
+    yellowLight.style.backgroundColor = 'yellow';
+    if(isYellow){
+    yellowLight.style.backgroundColor = 'yellow';
+    } else {
+        yellowLight.style.backgroundColor = '#444';
+    }
+    isYellow = !isYellow;
+});
+
+let isGreen = true;
+
+greenSwitch.addEventListener('click', () => {
+    greenLight.style.backgroundColor = 'green';
+    if(isGreen){
+    greenLight.style.backgroundColor = 'green';
+    } else {
+        greenLight.style.backgroundColor = '#444';
+    }
+    isGreen = !isGreen;
+});
+
+allLightsSwitch.addEventListener('click', () => {
+    redLight.style.backgroundColor = 'red';
+    if(isRed){
+    redLight.style.backgroundColor = 'red';
+    } else {
+        redLight.style.backgroundColor = '#444';
+    }
+    isRed = !isRed;
+    yellowLight.style.backgroundColor = 'yellow';
+    if(isYellow){
+    yellowLight.style.backgroundColor = 'yellow';
+    } else {
+        yellowLight.style.backgroundColor = '#444';
+    }
+    isYellow = !isYellow;
+    greenLight.style.backgroundColor = 'green';
+    if(isGreen){
+    greenLight.style.backgroundColor = 'green';
+    } else {
+        greenLight.style.backgroundColor = '#444';
+    }
+    isGreen = !isGreen;
+});
